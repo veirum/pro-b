ed9fb214c306c92641e1e7b6ed2e3d69412db90c
Jeppe Veirum Larsen <62033629+veirum@users.noreply.github.com>
2024-01-16 10:56:11 +0100

    Update

---
+++ Forløb/tic tac toe/tic_tac_toe_2024_p5_project/sketch.js
@@ -0,0 +1,15 @@
+//**************************************
+//* Tic tac toe forløb Programmering B *
+//* af Jeppe Veirum Larsen Januar 2024 *
+//**************************************
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //Det første
+
+  //TODO: Lav de 3 resterende streget til vores spilleplade
+}
