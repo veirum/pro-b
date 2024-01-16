1b6038e464b1c6557e81241178503c2d9c37bffa
Jeppe Veirum Larsen <62033629+veirum@users.noreply.github.com>
2024-01-16 11:52:38 +0100

    Update

--- Forløb/tic tac toe/tic_tac_toe_2024_p5_project/sketch.js
+++ Forløb/tic tac toe/tic_tac_toe_2024_p5_project/sketch.js
@@ -1,12 +1,12 @@
-//***********************************************************************
-//* Tic tac toe
+//*****************************************************************************
+//* Tic Tac Toe
 //* Programmering B,
 //* af Jeppe Veirum Larsen, Januar 2024
 //*
 //* README!
-//* Forløbet bruger TODO, under hver TODO er koden udkommenteret ved brug
-//* af /*  */. Fjern disse under hver TODO, men KUN som du kommer frem.
-//***********************************************************************
+//* Forløbet bruger TODO, under hver TODO er der TIP og OPGAVE. TIP kommer
+//* med små hints til hvad man KAN bruge, OPGAVE beskrive hvad der SKAL laves.
+//*****************************************************************************
 
 function setup() {
   createCanvas(400, 400);
@@ -14,27 +14,54 @@ function setup() {
 
 function draw() {
   background(220);
-  //TODO: Lav en spilleplade
-  line(width / 3, 0, Width / 3, height);
+  //TODO: Lav en spilleplade på 3 x 3 felter
+
+  //-------------------------------------------------
+  // TIP:                                           |
+  // line(width / 3, 0, width / 3, height);         |
+  //-------------------------------------------------
+
+  //-------------------------------------------------
+  // OPGAVE:                                        |
+  // Tegn 4 streger for at lave vores spilleplade   |
+  //-------------------------------------------------
+
+  // Skriv din kode her og slet mig!
 
   //TODO: Lav et X
-  /*
 
-  */
+  //-------------------------------------------------
+  // TIP:                                           |
+  // line() x 2;                                    |
+  // Tegn ud fra orego(0,0), tak mig senere         |
+  //-------------------------------------------------
+
+  //-------------------------------------------------
+  // OPGAVE:                                        |
+  // Tegn et X for at lave vores første spillebrik  |
+  //-------------------------------------------------
+
+  // Skriv din kode her og slet mig!
 
   //TODO: Lav et O
-  /*
 
-  */
+  //-------------------------------------------------
+  // TIP:                                           |
+  // circle() / ellipse()                           |
+  // Tegn ud fra orego(0,0), tak mig senere         |
+  //-------------------------------------------------
+
+  //-------------------------------------------------
+  // OPGAVE:                                        |
+  // Tegn et X for at lave vores første spillebrik  |
+  //-------------------------------------------------
+
+  // Skriv din kode her og slet mig!
 
   //TODO: Hvor er musen?
   /*
 
   */
 
-  //TODO: Tegn de resterende streger
-  //Skriv din egen kode herunder
-  /*
-
-  */
+  //TODO: Spillebrikker som en Funktion
 }
